74fb6de81ad3f2136622f0e6deb77c15528011db
LzKelvinSoares <kelvinsoaresklv@gmail.com>
2025-06-30 18:24:47 +0100

    - Add eas

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -33,13 +33,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'Settings',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="settings.fill" color={color} />,
-        }}
-      />
     </Tabs>
   );
 }
